797dc633021c03ede5b27707ea03c7ee5a910d56
Hanson <201173629@qq.com>
2020-10-09 20:44:22 +0800

    事件

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,56 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    title: ['Han', 'Son', 'Hanson']
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  handleClick() {
+    console.log('点击了按钮')
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+  handleTouchStart() {
+    console.log('handleTouchStart')
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
+  handleTouchMove() {
+    console.log('handleTouchMove')
+  },
+  handleTouchEnd() {
+    console.log('handleTouchEnd')
+  },
+  handleTap() {
+    console.log('handleTap')
+  },
+  handleLongpress() {
+    console.log('handleLongpress')
+  },
+  handleEventClick(event) {
+    console.log('---', event)
+  },
+  handleEventEnd(event) {
+    console.log('+++', event)
+  },
+  handleFor(event) {
+    // console.log(event);
+    var res = event.currentTarget.dataset;
+    console.log(res.item)
+  },
+  // 事件冒泡和事件捕获
+  handleCaptureView1() {
+    console.log('handleCaptureView1')
+  },
+  handleCaptureView2() {
+    console.log('handleCaptureView2')
+  },
+  handleCaptureView3() {
+    console.log('handleCaptureView3')
+  },
+  handleBindView1() {
+    console.log('handleBindView1')
+  },
+  handleBindView2() {
+    console.log('handleBindView2')
+  },
+  handleBindView3() {
+    console.log('handleBindView3')
+  },
 })
