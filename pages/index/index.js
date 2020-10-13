177547bb4a04af8f68b98bf21f5082558372a38f
Hanson <201173629@qq.com>
2020-10-13 21:02:16 +0800

    弹窗和分享事件

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,68 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+  onToastClick() {
+    wx.showToast({
+      title: '等待一下',
+      // 时间
+      duration: 3000,
+      icon: 'loading',
+      // 加一层蒙版，防止点击此按钮后还可以点击其他按钮
+      mask: true,
+      success: function() {
+        console.log('展示成功')
+      },
+      fail: function() {
+        console.log('展现失败')
+      },
+      complete: function() {
+        console.log('完成函数的调用')
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
     })
+  },
+  onModalClick() {
+    wx.showModal({
+      title: '我是标题',
+      content: '我是内容',
+      // 是否显示取消的
+      showCancel: true,
+      cancelText: '退出',
+      cancelColor: 'red',
+      success: function(res) {
+        console.log(res)
+        if(res.confirm) {
+          console.log('用户点击了确定')
+        }
+        if(res.cancel) {
+          console.log('用户点击了退出')
+        }
+      }
+    })
+  },
+  onLoadingClick() {
+    wx.showLoading({
+      title: '等待中',
+      // 加一层蒙版，防止点击此按钮后还可以点击其他按钮
+      mask: true,
+    })
+    setTimeout(() => {
+      //必须调用隐藏方法才能让loading方法消失
+        wx.hideLoading()
+    }, 2000);
+  },
+  onActionSheeClick() {
+    wx.showActionSheet({
+      itemList: ['相册','拍照'],
+      itemColor: '#ff0000',
+      success: function(res) {
+        console.log(res)
+      }
+    })
+  },
+  // 分享函数
+  onShareAppMessage: function() {
+    return{
+      title: 'Hanson',
+      path: '/pages/index/index',
+      imageUrl: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3682983012,853466614&fm=26&gp=0.jpg'
+    }
   }
 })
